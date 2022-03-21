65b976c8fa40bed4654322116566d65bdf97a523
RalfG <ralfg@hotmail.be>
2022-03-21 14:49:25 +0100

    Update ms2pip part

--- scripts/ms2pip_utils.py
+++ scripts/ms2pip_utils.py
@@ -9,7 +9,7 @@ def reshape_attributes(attributes):
     return {item["name"]: item["value"] for item in attributes}
 
 
-def plot_unannotated_usi_spectrum(usi, peptide=None):
+def get_usi_spectrum(usi):
     proxi_response = proxi(usi)
     proxi_attributes = reshape_attributes(proxi_response["attributes"])
 
@@ -26,14 +26,58 @@ def plot_unannotated_usi_spectrum(usi, peptide=None):
         )
         .filter_intensity(0.01, 50)
     )
-
-    plt.figure(figsize=(12,6))
-    plt.title(usi)
-    sup.spectrum(spectrum)
-    plt.show()
+    return spectrum
 
 
-def plot_theoretical_spectrum(peptide): 
-    ms2pip = SinglePrediction()
-    mz, _, annotations = ms2pip.predict("VLHPLEGAVVIIFK", modifications="-", charge=2)
-    #todo
+def get_theoretical_spectrum(peptide, modifications, charge, b_ion_weight=1.0, ms2pip_instance=None):
+    if not ms2pip_instance:
+        ms2pip = SinglePrediction()
+    else:
+        ms2pip = ms2pip_instance
+
+    mz, _, annotation = ms2pip.predict(peptide, modifications, charge)
+    intensity = [b_ion_weight if ann[0] == "b" else 1.0 for ann in annotation]
+
+    identifier = f"{peptide}/{charge}/{modifications}"
+    precursor_mz = ms2pip.mod_info.calc_precursor_mz(peptide, modifications, charge)
+    mod_dict = ms2pip._modifications_to_dict(modifications)
+    sus_annotation = ms2pip._get_sus_annotation(mz, annotation)
+
+    spectrum = sus.MsmsSpectrum(
+        identifier,
+        precursor_mz,
+        charge,
+        mz,
+        intensity,
+        annotation=sus_annotation,
+        peptide=peptide,
+        modifications=mod_dict,
+    )
+    return spectrum
+
+
+def get_predicted_spectrum(peptide, modifications, charge, ms2pip_instance=None):
+    if not ms2pip_instance:
+        ms2pip = SinglePrediction()
+    else:
+        ms2pip = ms2pip_instance
+        
+    mz, intensity, annotation = ms2pip.predict(peptide, modifications, charge)
+    
+    identifier = f"{peptide}/{charge}/{modifications}"
+    precursor_mz = ms2pip.mod_info.calc_precursor_mz(peptide, modifications, charge)
+    mod_dict = ms2pip._modifications_to_dict(modifications)
+    sus_annotation = ms2pip._get_sus_annotation(mz, annotation)
+
+    spectrum = sus.MsmsSpectrum(
+        identifier,
+        precursor_mz,
+        charge,
+        mz,
+        intensity,
+        annotation=sus_annotation,
+        peptide=peptide,
+        modifications=mod_dict,
+    )
+    return spectrum
+
